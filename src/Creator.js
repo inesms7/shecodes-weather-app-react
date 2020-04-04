0df176c3c7110468822f3831f467056aa25c17c8
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 11:57:15 +0100

    Create Creator.js

---
+++ src/Creator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Creator() {
+	return (
+		<div>
+			<p className="creator">
+				<a href="https://github.com/inesms7/shecodes-weather-app">
+					Open-source code
+				</a>
+				by Inês Santos
+			</p>
+		</div>
+	);
+}
