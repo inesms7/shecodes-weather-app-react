baffe7945983c0a0be26a0c030dc460c8ce47917
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 11:54:55 +0100

    App.js code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+/*import Creator from "./Creator";*/
+import Forecast from "./Forecast";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+	return (
+		<div className="wrapper">
+			<WeatherSearch />
+			<p id="date">10 February, 2020</p>
+			<p id="time">10:00 Monday</p>
+
+			<div>
+				<div>
+					<div className="button-current-location" id="location">
+						Current Location
+					</div>
+					<div />
+				</div>
+				<div className="col">
+					<h4>
+						A GREAT DAY TO GO FOR A WALK <span> 😎</span>
+					</h4>
+				</div>
+			</div>
+			<Forecast />
+		</div>
+		/* <div>
+            <Creator />   
+      </div>  
+    */
+	);
+}
