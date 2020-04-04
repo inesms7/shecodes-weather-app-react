00106475d32f8777de4acef343137e349c68fb11
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 11:55:37 +0100

    Create Forecast.js

---
+++ src/Forecast.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function Forecast() {
+    return (
+        <div className="container" id="container_forecast">
+            <p className="forecast">NEXT HOURS:</p>
+
+            <div className="row" id="forecast-input">
+                <div className="col">
+                    <p className="forecast-p">Sun</p>
+                    <img
+                        src="https://home.e-inscricao.com/wp-content/uploads/2015/10/sun1.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+                <div className="col">
+                    <p className="forecast-p">Mon</p>
+                    <img
+                        src="https://home.e-inscricao.com/wp-content/uploads/2015/10/sun1.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+                <div className="col">
+                    <p className="forecast-p">Tue</p>
+                    <img
+                        src="http://www.free-icons-download.net/images/a-thunderstorm-icon-38671.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+                <div className="col">
+                    <p className="forecast-p">Wed</p>
+                    <img
+                        src="https://home.e-inscricao.com/wp-content/uploads/2015/10/sun1.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+                <div className="col">
+                    <p className="forecast-p">Thu</p>
+                    <img
+                        src="http://www.free-icons-download.net/images/a-thunderstorm-icon-38671.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+
+                <div className="col">
+                    <p className="forecast-p">Fri</p>
+                    <img
+                        src="https://home.e-inscricao.com/wp-content/uploads/2015/10/sun1.png"
+                        alt=""
+                        className="forecast-img"
+                    />
+                    <div className="forecast-temp">
+                        <strong>22°</strong> 17°
+          </div>
+                </div>
+            </div>
+        </div>
+    );
+}
